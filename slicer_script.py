0d5c789cd496849f48a3ce13a6dc098e425339aa
Laura Connolly <15lpc1@queensu.ca>
2025-05-11 13:25:47 -0400

    Updating the slicer script file.

--- slicer_script.py
+++ slicer_script.py
@@ -1,9 +1,11 @@
 transform = slicer.mrmlScene.GetFirstNodeByName('TumorTransform')
 ros = slicer.util.getModuleLogic('ROS2')
 node = ros.GetDefaultRos2Node()
-publisher = node.CreateAndAddPublisher('vtkMRMLROS2PoseNode', 'tumor_transform')
+publisher = node.CreateAndAddPublisherNode('Pose', 'tumor_transform_topic')
 
-def updatetransform():
+def updateTransform():
   matrix = vtk.vtkMatrix4x4()
   transform.GetMatrixTransformToParent(matrix)
   publisher.Publish(matrix)
+
+transform.AddObserver(vtk.vtkMRMLTransformNode.TransformModifiedEvent, updateTransform)
